04dc4dfcf8505e7dee9dedfc478b2575852f5fef
Eric Dennison <ericd@netdenizen.com>
2015-08-07 13:39:21 -0400

    Updated from Brython Server: 8/7/2015 1:39:20 PM

--- spacewar.py
+++ spacewar.py
@@ -1,1 +1,14 @@
-print("Hello new world")
+from ggame import App, Sprite, ImageAsset
+
+class Stars(Sprite):
+    
+    def __init__(self, position):
+        super().__init__(ImageAsset("starfield.jpg"), position)
+        
+class Spacewar(App):
+    
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.stars = Stars((0,0))
+        
+Spacewar(800,600).run()
