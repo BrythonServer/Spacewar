704d91ffa8ddd4d3f7c098fc51da1a7274ca703c
Eric Dennison <ericd@netdenizen.com>
2015-08-07 14:20:06 -0400

    Updated from Brython Server: 8/7/2015 2:20:06 PM

--- spacewar.py
+++ spacewar.py
@@ -4,6 +4,8 @@ from ggame import App, Sprite, ImageAsset
 class Stars(Sprite):
 
     asset = ImageAsset("starfield.jpg")
+    width = 512
+    height = 512
 
     def __init__(self, position):
         super().__init__(Stars.asset, position)
@@ -11,6 +13,8 @@ class Stars(Sprite):
 class Sun(Sprite):
     
     asset = ImageAsset("sun.png")
+    width = 80
+    height = 76
     
     def __init__(self, position):
         super().__init__(Sun.asset, position)
@@ -19,23 +23,15 @@ class Spacewar(App):
     
     def __init__(self, width, height):
         super().__init__(width, height)
-        self.star1 = Stars((0,0))
-        sun = Sun((400,300))
-        sunw = sun.width
-        sunh = sun.height
-        print(sunw, sunh)
-        #for x in range(width//sw + 1):
-        #    for y in range(height//sh + 1):
-        #        #print(x*sw, y*sh)
-        #        Stars((x*sw, y*sh))
-        #star1.destroy()
+        for x in range(width//Stars.width + 1):
+            for y in range(height//Stars.height + 1):
+                Stars((x*Stars.width, y*Stars.height))
+        sun = Sun((width/2 - Sun.width/2, height/2 - Sun.height/2))
+
         
     def step(self):
-        sw = self.star1.width
-        sh = self.star1.height
-        #star1.destroy()
-        print(sw, sh)
-
+        pass
+    
 app = Spacewar(800,600)
 app.run()
 
