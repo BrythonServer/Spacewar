b83332945c73921485ba6f02405bb7e63a35b463
Eric Dennison <ericd@netdenizen.com>
2015-08-07 13:57:30 -0400

    Updated from Brython Server: 8/7/2015 1:57:29 PM

--- spacewar.py
+++ spacewar.py
@@ -15,10 +15,12 @@ class Spacewar(App):
         star1 = Stars((0,0))
         sw = star1.width
         sh = star1.height
-        for x in range(width//sw + 1):
-            for y in range(height//sh + 1):
-                #print(x*sw, y*sh)
-                Stars((x*sw, y*sh))
+        star1.destroy()
+        print(sw, sh)
+        #for x in range(width//sw + 1):
+        #    for y in range(height//sh + 1):
+        #        #print(x*sw, y*sh)
+        #        Stars((x*sw, y*sh))
         #star1.destroy()
         
 app = Spacewar(800,600)
