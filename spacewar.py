3de47880cc2f6ed57037595f860a51e827dd13c1
Eric Dennison <ericd@netdenizen.com>
2015-08-07 18:43:04 -0400

    Updated from Brython Server: 8/7/2015 6:43:03 PM

--- spacewar.py
+++ spacewar.py
@@ -96,7 +96,7 @@ class Bullet(GravitySprite):
                 self.visible = False
             ships = []
             ships = self.collidingWithSprites(Ship1)
-            ships.extend(self.collidingWithSprites(Ship2)
+            ships.extend(self.collidingWithSprites(Ship2))
             #ships = self.collidingWithSprites(Ship1).extend(self.collidingWithSprites(Ship2))
             if len(ships):
                 if not self.firing:
