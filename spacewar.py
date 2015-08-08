07e6003c3cb72625c516c4f54ed52e901a510dcd
Eric Dennison <ericd@netdenizen.com>
2015-08-08 11:08:37 -0400

    Updated from Brython Server: 8/8/2015 11:08:37 AM

--- spacewar.py
+++ spacewar.py
@@ -68,8 +68,8 @@ class GravitySprite(Sprite):
         dt2o2 = dT*dT*0.5
         self.vx += (Agx + Tt*math.sin(self.rotation))* dT
         self.vy += (Agy - Tt*math.cos(self.rotation))* dT
-        self.x += self.vx + Agx*dt2o2
-        self.y += self.vy + Agy*dt2o2
+        self.x += self.vx * dT + Agx*dt2o2
+        self.y += self.vy * dT + Agy*dt2o2
 
 
 class Bullet(GravitySprite):
